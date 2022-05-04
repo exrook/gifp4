582a10665e09c7d87d04300234f5719b94b98bf2
Jacob Hughes <j@distanthills.org>
2022-05-04 01:01:12 -0400

    add Content-Type header
    fix XSS issues (hopefully)
    remove compression because of c depedency
    make links relative
    probably some other things idk

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,11 @@ use std::str::Utf8Error;
 
 type ID = [u8; 6];
 
-fn cdn_suffix(url: &str) -> Option<&str> {
-    url.split_once("discordapp.com/attachments/")
-        .map(|(_, s)| s)
+fn cdn_suffix(url: &str) -> Option<String> {
+    let s: String = dbg!(ammonia::Url::parse(url)).ok()?.into();
+    s.split_once("discordapp.com/attachments/")
+        .or(url.split_once("discordapp.net/attachments/"))
+        .map(|(_, s)| s.to_owned())
 }
 
 fn parse_id(id: &str) -> Option<ID> {
@@ -40,7 +42,6 @@ fn insert(db: &Db, gif: &str, mp4: &str) -> sled::Result<ID> {
             return Err(sled::Error::Unsupported("out of IDs".to_owned()));
         }
     };
-    println!("{:?}", id);
     let gif_key = append_key!(id, 1);
     let mp4_key = append_key!(id, 2);
     let mut batch = Batch::default();
@@ -84,55 +85,81 @@ fn lookup(db: &Db, id: &ID) -> sled::Result<Option<(IString, IString)>> {
 
 use http_body::{combinators::BoxBody, Full};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::Uri;
 use hyper::{
     body::{Buf, Bytes, HttpBody},
-    service::{Service},
-    Body, Method, Request, Response, Server, StatusCode
+    service::Service,
+    header::CONTENT_TYPE,
+    Body, Method, Request, Response, Server, StatusCode,
 };
-use hyper::Uri;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
 type MyBody = BoxBody<Box<dyn Buf + 'static + Send + Sync>, hyper::Error>;
 
 async fn serve(db: &Db, req: Request<Body>) -> Result<Response<MyBody>, hyper::Error> {
-    let id = req.uri().path().strip_prefix("/").and_then(parse_id);
+    let id = req
+        .uri()
+        .path()
+        .strip_prefix("/")
+        .map(|s| s.split_once(".").map(|(p, _)| p).unwrap_or(s))
+        .and_then(parse_id);
     match (req.method(), req.uri().path(), id) {
         (&Method::GET, "/", None) => send_home(),
         (&Method::POST, "/submit", None) => Ok(submit_request(db, req).await.unwrap()),
         (&Method::GET, _, Some(id)) => Ok(generate_response(db, &id).await.unwrap()),
         _ => not_found(),
-    }
+    }.map(|mut resp| {
+        resp.headers_mut().insert(CONTENT_TYPE, "text/html; charset=utf-8".parse().unwrap());
+        resp
+    })
 }
 
+use std::collections::HashMap;
 use warp::Filter;
 use warp::Reply;
-use std::collections::HashMap;
 
 async fn submit_request(db: &Db, req: Request<Body>) -> Result<Response<MyBody>, hyper::Error> {
-    let route = warp::body::content_length_limit(1024).and(warp::body::form()).map(|form: HashMap<String, String>| {
-        if let Some((gif, mp4)) = form.get("gif").and_then(|s|cdn_suffix(s)).and_then(|gif|form.get("mp4").and_then(|s|cdn_suffix(s)).map(|mp4|(gif, mp4))) {
-            match insert(db, gif, mp4) {
-                Ok(id) => {
-                    let uri = Uri::builder().path_and_query(format!("/{}", format_id(&id))).build().unwrap();
-                    warp::redirect::see_other(uri).into_response()
+    let route = warp::body::content_length_limit(1024)
+        .and(warp::body::form())
+        .map(|form: HashMap<String, String>| {
+            if let Some((gif, mp4)) = form.get("gif").and_then(|s| cdn_suffix(s)).and_then(|gif| {
+                form.get("mp4")
+                    .and_then(|s| cdn_suffix(s))
+                    .map(|mp4| (gif, mp4))
+            }) {
+                match insert(db, &gif, &mp4) {
+                    Ok(id) => {
+                        let uri = Uri::builder()
+                            .path_and_query(format!("{}", format_id(&id)))
+                            .build()
+                            .unwrap();
+                        warp::redirect::see_other(uri).into_response()
+                    }
+                    Err(e) => {
+                        eprintln!("Error inserting: {:?}", e);
+                        warp::reply::with_status(
+                            warp::reply::reply(),
+                            warp::http::StatusCode::INTERNAL_SERVER_ERROR,
+                        )
+                        .into_response()
+                    }
                 }
-                Err(e) => {
-                    eprintln!("Error inserting: {:?}", e);
-                    warp::reply::with_status(warp::reply::reply(), warp::http::StatusCode::INTERNAL_SERVER_ERROR).into_response()
-                }
+            } else {
+                warp::reply::with_status(warp::reply::reply(), warp::http::StatusCode::BAD_REQUEST)
+                    .into_response()
             }
-        } else {
-            warp::reply::with_status(warp::reply::reply(), warp::http::StatusCode::BAD_REQUEST).into_response()
-        }
-    });
+        });
     let (parts, body) = warp::service(route).call(req).await.unwrap().into_parts();
-    Ok(Response::from_parts(parts, BoxBody::new(body.map_data(|d|Box::new(d) as Box<dyn Buf + 'static + Send + Sync>))))
+    Ok(Response::from_parts(
+        parts,
+        BoxBody::new(body.map_data(|d| Box::new(d) as Box<dyn Buf + 'static + Send + Sync>)),
+    ))
 }
 
 static P1: &'static str = &r#"<!DOCTYPE html><html><head><meta property="og:image" content="https://cdn.discordapp.com/attachments/"#;
 static P2: &'static str = &r#""><meta property="og:type" content="video.other"><meta property="og:video:url" content="https://cdn.discordapp.com/attachments/"#;
-static P3: &'static str = &r#""><meta property="og:video:height" content="202"><meta property="og:video:width" content="250"></head><body><a href="/">Submit</a></body></html>"#;
+static P3: &'static str = &r#""><meta property="og:video:height" content="202"><meta property="og:video:width" content="250"></head><body><img src="https://cdn.discordapp.com/attachments/963891685471957103/967247400840335410/trollface_PNG30.png"></body></html>"#;
 
 use std::io::Cursor;
 
@@ -146,32 +173,31 @@ fn make_page_response(gif: IString, mp4: IString) -> impl Buf + 'static + Send +
 
 async fn generate_response(db: &Db, id: &ID) -> Result<Response<MyBody>, ()> {
     let (gif, mp4) = lookup(db, id).ok().flatten().ok_or(())?;
-    Ok(Response::new(MyBody::new(Full::new(
-        Box::new(make_page_response(gif, mp4)) as Box<_>,
-    ).map_err(|_|todo!()))))
+    Ok(Response::new(MyBody::new(
+        Full::new(Box::new(make_page_response(gif, mp4)) as Box<_>).map_err(|_| todo!()),
+    )))
 }
 
 static HOME_PAGE: &'static str = include_str!("index.html");
 
 fn send_home() -> Result<Response<MyBody>, hyper::Error> {
-    Ok(Response::new(BoxBody::new(Full::new(
-        Box::new(HOME_PAGE.as_bytes()) as _,
-    ).map_err(|_|todo!()))))
+    Ok(Response::new(BoxBody::new(
+        Full::new(Box::new(HOME_PAGE.as_bytes()) as _).map_err(|_| todo!()),
+    )))
 }
 
 static NOT_FOUND: &'static str = include_str!("not_found.html");
 
 fn not_found() -> Result<Response<MyBody>, hyper::Error> {
-    let mut resp = Response::new(BoxBody::new(Full::new(Box::new(NOT_FOUND.as_bytes()) as _).map_err(|_|todo!())));
+    let mut resp = Response::new(BoxBody::new(
+        Full::new(Box::new(NOT_FOUND.as_bytes()) as _).map_err(|_| todo!()),
+    ));
     *resp.status_mut() = StatusCode::NOT_FOUND;
     Ok(resp)
 }
 
 fn database() -> sled::Result<Db> {
-    sled::Config::new()
-        .path("gifp4_db")
-        .use_compression(true)
-        .open()
+    sled::Config::new().path("gifp4_db").open()
 }
 
 #[tokio::main]
